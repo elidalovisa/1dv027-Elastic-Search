092fbdea411af7b1696416ca4d5310414865303b
Elida Arrechea <elida@Elidas-MacBook-Air.local>
2022-05-17 13:17:48 +0200

    add eslint

--- src/App.js
+++ src/App.js
@@ -2,11 +2,6 @@ import './App.css';
 
 import Graph from './components/Graph.js';
 
-
-// GET ALL (set size) /argentina/_search?size=30&q=*:*
-// /argentina/_search?pretty&q=TimeStamp:2022-05-13
-// /argentina/_search?pretty
-//https://sxr5w7jebl:ihql44kfcj@apple-279251793.eu-west-1.bonsaisearch.net:443
 function App() {
   return (
     <div className="App">
